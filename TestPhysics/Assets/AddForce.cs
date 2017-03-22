e022a19e571706e255983b0299ead2c3d4c885a6
MIKIEJ-DESKTOP\mikiej <mikiej_boy@hotmail.com>
2017-03-22 21:23:45 +0000

    Initial Commit of porject - basic set up of ball being shot at bricks

---
+++ TestPhysics/Assets/AddForce.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class AddForce : MonoBehaviour {
+
+    void OnMouseDown()
+    {
+
+        GetComponent<Rigidbody>().AddForce(-transform.forward * 5000, ForceMode.Acceleration);
+        GetComponent<Rigidbody>().useGravity = true;
+    }
+
+}
